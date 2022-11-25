bcee4fdf1992b98271bb3aed0a3764a610f84892
Ugege Daniel <ugege62@gmail.com>
2022-11-25 20:00:45 +0100

    socials

--- src/Components/home/Socials.jsx
+++ src/Components/home/Socials.jsx
@@ -1,32 +1,17 @@
 import React from 'react'
-import { AiFillGithub, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai'
-import { FaEnvelope } from 'react-icons/fa'
+import { socials } from '../../utils'
 import './styles/Socials.css';
 
 const Socials = () => {
   return (
     <div className='socials'>
       <div className="social">
-        <a href="https://linkedin.com/in/favour-faiyetole" target='_blank' rel='noreferrer'>
-          <div className='link ml'>
-            <FaEnvelope />
+        {socials.map((social, index)=><a href={social.link} target='_blank' rel='noreferrer' key={index}>
+          <div className={social.className}>
+            <social.icon />
           </div>
         </a>
-        <a href="https://github.com/Dun-sin" target='_blank' rel='noreferrer'>
-          <div className='git ml'>
-            <AiFillGithub />
-          </div>
-        </a>
-        <a href="https://linkedin.com/in/favour-faiyetole" target='_blank' rel='noreferrer'>
-          <div className='link ml'>
-            <AiFillLinkedin />
-          </div>
-        </a>
-        <a href="https://twitter.com/DunsinWebDev" target='_blank' rel='noreferrer'>
-          <div className='twit ml'>
-            <AiFillTwitterCircle />
-          </div>
-        </a>
+        )}
       </div>
     </div>
   )
---
+++ src/utils/socials.js
@@ -0,0 +1,27 @@
+import { FaEnvelope } from 'react-icons/fa'
+import { AiFillGithub, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai'
+
+const socials = [
+    {
+        link: "https://linkedin.com/in/favour-faiyetole",
+        icon: FaEnvelope,
+        className: 'link ml'
+    },
+    {
+        link: "https://github.com/Dun-sin",
+        icon: AiFillGithub,
+        className:'git ml'
+    },
+    {
+        link: "https://linkedin.com/in/favour-faiyetole",
+        icon: AiFillLinkedin,
+        className: 'link ml'
+    },
+    {
+        link: "https://twitter.com/DunsinWebDev",
+        icon: AiFillTwitterCircle,
+        className: 'twit ml'
+    },
+]
+
+export default socials
