14bb54c22161925dcfbf7f842b9cc4cbb55776d9
Ugege Daniel <ugege62@gmail.com>
2022-11-27 11:46:16 +0100

    responsivesness

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -2,14 +2,12 @@ import { HeadlineText, TypeWritterEffectText } from './index'
 import './styles/Home.css'
 import { HomeCta, Socials, Skills } from './index'
 const Home = ({ section }) => (
-  <div className='home' id='home' ref={section}>
-    <div className="home-text">
+    <div className="home-text" id='home' ref={section}>
       <HeadlineText />
       <TypeWritterEffectText />
       <HomeCta />
       <Socials />
       <Skills />
     </div>
-  </div >
 )
 export default Home;
